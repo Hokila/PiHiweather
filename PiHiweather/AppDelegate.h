681eab45997f9270f8d382617e1228dda741ea05
Hokila <yseric@gmail.com>
2013-05-25 16:01:44 +0800

    Initial Commit

---
+++ PiHiweather/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PiHiweather
+//
+//  Created by Hokila on 13/5/25.
+//  Copyright (c) 2013年 PiHi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PiHiweather/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PiHiweather
+//
+//  Created by Hokila on 13/5/25.
+//  Copyright (c) 2013年 PiHi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
